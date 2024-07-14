193062c166d097315da182faf4b5cff3f0f33cb6
Thanosrom <thanosrom95@gmail.com>
2024-07-14 17:38:55 +0300

    Changed the version 7-14-2024

--- Controllers/Check_App_Version_Controller.js
+++ Controllers/Check_App_Version_Controller.js
@@ -4,7 +4,7 @@ const express = require('express');
 const check_App_Version_Controller = {
   async check_App_Version(req, res) {
     try {
-      res.status(200).json('1.0.0+7');
+      res.status(200).json('1.0.0+9');
     } catch (error) {
       res
         .status(400)

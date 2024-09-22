e97709038c263f95e951f7e264e0af1318afef04
thanosrom <thanosrom95@gmail.com>
2024-09-22 13:41:21 +0000

    All Complete ! Only Layout left

--- Controllers/Check_App_Version_Controller.js
+++ Controllers/Check_App_Version_Controller.js
@@ -4,7 +4,7 @@ const express = require('express');
 const check_App_Version_Controller = {
   async check_App_Version(req, res) {
     try {
-      res.status(200).json('1.0.0+16');
+      res.status(200).json('1.0.0+17');
     } catch (error) {
       res
         .status(400)
